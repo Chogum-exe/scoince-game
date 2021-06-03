d22d364eedb3e27faf07edef1d0c072f91c2709b
Syntaxd <potatoslalom@gmail.com>
2021-06-03 15:47:36 -0700

    Somewhat able to move to app folder

--- build.py
+++ build.py
@@ -1,10 +1,21 @@
 import subprocess
-
-amazon = ['pygame', 'pyinstaller']
-
+import os
+amazon = ['pygame', 'pyinstaller', 'shutil']
 for package in amazon:
-    subprocess.call(['py', '-m', 'pip', 'install', package])
+    subprocess.call(['python', '-m', 'pip', 'install', package])
 
 print('Spawning new python window')
-subprocess.call(['py', './main.py'])
+# subprocess.call(['python', 'main.py'])
+
+print(os.name)
+if os.name == 'nt':
+    APP_PATH = "C:/Somthing/Somthing/Soemthing"
+if os.name = 'posix'
+    APP_PATH = "Path/Path/thing"
+
+destination = shutil.move(os.cwd, APP_PATH, copy_function = shutil.copytree)
+print('moved to: ' + destination)
+# if os.name
+
 # subprocess.call(['pyinstaller', '--onefile', 'main.py'])
+
