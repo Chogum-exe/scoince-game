91b3d1a9759f7e9fa8521a1588537505b654a26d
Soooham <75951802+Chogum-exe@users.noreply.github.com>
2021-06-03 15:23:12 -0700

    Single Character Frames Added

--- Game/Player/Python/controler.py
+++ Game/Player/Python/controler.py
@@ -1,7 +1,7 @@
 from pygame import key
 class Key(object):
     def wasd():
-        le  = pygame.key.get_pressed()[pygame.K_w] or pygame.key.get_pressed()[pygame.K_UP]
+        left  = pygame.key.get_pressed()[pygame.K_w] or pygame.key.get_pressed()[pygame.K_UP]
         up    = pygame.key.get_pressed()[pygame.K_a] or pygame.key.get_pressed()[pygame.K_LEFT]
         down  = pygame.key.get_pressed()[pygame.K_s] or pygame.key.get_pressed()[pygame.K_DOWN]
         right = pygame.key.get_pressed()[pygame.K_d] or pygame.key.get_pressed()[pygame.K_RIGHT]
