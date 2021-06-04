a866a93a9117e92d91bca8a4fdffe10fafa1b1f8
Sytaxd <potatoslalom@gmail.com>
2021-06-03 20:49:37 -0700

    Allow support for windows users

--- build.py
+++ build.py
@@ -3,9 +3,12 @@ import os
 import shutil
 amazon = ['pygame', 'pyinstaller', 'shutil']
 for package in amazon:
-    subprocess.call(['python', '-m', 'pip', 'install', package])
-
-print('Game Builder: Spawning new python window')
+    if os.name == 'nt':
+        subprocess.call(['py', '-m', 'pip', 'install', package])
+    else:
+        subprocess.call(['python', '-m', 'pip', 'install', package])
+#
+# print('Game Builder: Spawning new python window')
 # subprocess.call(['python', 'main.py'])
 
 print(os.name)
@@ -15,6 +18,7 @@ if os.name == 'posix':
     APP_PATH = "/Applications"
 
 destination = shutil.move('/Users/ryan/Documents/GitHub/Host/scoince-game', APP_PATH, copy_function = shutil.copytree)
+# os.getcwd()
 print('Game Builder: moved to: ' + destination)
 
 
