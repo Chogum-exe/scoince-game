4fd56e6543dd806512446629a3ff7fcd293605bf
Sytaxd <potatoslalom@gmail.com>
2021-06-04 18:00:16 -0700

    Yay its working

--- ScienceGame/Game/Questions/Question.py
+++ ScienceGame/Game/Questions/Question.py
@@ -1,15 +1,20 @@
-from json import load
-from random import gauss
-from random import randint
+import json
+import numpy as np
+import random
 
 def GenerateQuestions(type, topic, weight, falloff):
     # type = (MultipleChoice or TrueFalse
     # topic = (Biology, Chemistry, Computer, or Earth)
     QuestionFile = open(type + '/' + topic + '.json', 'r')
     JsonData = json.load(QuestionFile)
-    WeightedRandom = round(random.guass(weight, falloff), 1)
+
+    WeightedRandom = str(round(np.random.normal(weight, falloff), 0))
+    print(WeightedRandom)
+
     GeneratedQuestion = JsonData[WeightedRandom]
     if len(GeneratedQuestion) > 0:
-        return GeneratedQuestion[random.randint(0, len(GeneratedQuestion))]
+        return random.choice(GeneratedQuestion)
+        # random.randint(0, len(GeneratedQuestion))
     else:
         return GeneratedQuestion
+print(GenerateQuestions('MultipleChoice', 'Computer', 1, 0))
