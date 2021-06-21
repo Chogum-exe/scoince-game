121c6ca6c5fc64ea904998239018110161b857e6
Sytaxd <potatoslalom@gmail.com>
2021-06-21 15:21:43 -0700

    More optimized rendering (pt 2/3)?

--- ScienceGame/Game/Map/background.py
+++ ScienceGame/Game/Map/background.py
@@ -23,7 +23,6 @@ class ScrollingBackround():
         for x in range(X_TILE_COUNT):
             for y in range(Y_TILE_COUNT):
                 self.cache[(x,y)] = self._load(x, y)
-
     def _load(self, x, y):
         img_path = os.path.join(self.path, 'images{}-{}.png'.format(x, y))
         return pygame.image.load(img_path).convert()
@@ -31,19 +30,16 @@ class ScrollingBackround():
     def _sector(self, x_offset, y_offset, screen_x, screen_y):
         left   = math.floor(x_offset / self.tilesize)
         top    = math.floor(y_offset / self.tilesize)
-
         right  = math.floor((screen_x + x_offset)/ self.tilesize)
-
         bottom = math.floor((screen_y + y_offset) / self.tilesize)
         return (left, top, right, bottom)
-        # Divide screen by 2?
 
-    # def _tile(self, x, y, screen_x, screen_y):
-    #     s = _sector(x, y, screen_x, screen_y)
-    #     for x_pos in range(s[0]):
-    #         for y_pos in range(s[1]):
-    #             if not self.cache[(x_pos, y_pos)]:
-    #                 self.cache[(x_pos, y_pos)] = _load(x_pos, y_pos)
+    def _tile(self, x, y, screen_x, screen_y, pos):
+        left, top, right, bottom = pos
+        for x_pos in range(left, right + 1):
+            for y_pos in range(top, bottom + 1):
+                if self.cache.get((x_pos, y_pos)) is None:
+                    self.cache[(x_pos, y_pos)] = self._load(x_pos, y_pos)
 
 
     def _draw_box(self, screen, x_offset, y_offset, x, y):
@@ -67,49 +63,15 @@ class ScrollingBackround():
             (x*self.tilesize-x_offset, y*self.tilesize+self.tilesize-y_offset),
             (x*self.tilesize+self.tilesize-x_offset, y*self.tilesize+self.tilesize-y_offset),
         )
-
     def show(self, screen, x_offset, y_offset):
         screen_x, screen_y = screen.get_size()
-        left, top, right, bottom = self._sector(x_offset, y_offset, screen_x, screen_y)
+
+        s = self._sector(x_offset, y_offset, screen_x, screen_y)
+        self._tile(x_offset, y_offset, screen_x, screen_y, s)
+
+        left, top, right, bottom = s
         for x in range(left, right + 1):
             for y in range(top, bottom + 1):
                 img = self.cache[(x,y)]
                 screen.blit(img, (x*self.tilesize-x_offset, y*self.tilesize-y_offset))
-                # self._draw_box(screen, x_offset, y_offset, x, y)
-
-
-# ============================================================================
-
-class ScrollingBackround1():
-    def __init__(self, path, tilesize):
-        self.path = path
-        self.tilesize = tilesize
-        self.cache = {}
-    def _load(self, img, x, y):
-         # PilString = image.tostring("raw", strFormat)
-         # return pygame.image.fromstring(raw_str, image.size, strFormat)
-        self.img = pygame.image.load(img).convert()
-        self.img = pygame.transform.scale(self.img, (x, y))
-        return self.img
-    def _size(self, img):
-        self.tileWidth = get_height(img)
-        self.tileWidth = get_width(img)
-    # def _tile(self, x, y):
-    #     return [self.stagePosY % (self.tileHeight * len(self.tiles)) // self.tileHeight, #x1
-    #             self.stagePosX % (self.tileWidth * len(self.tiles[0])) // self.tileWidth], # y1
-    #             [self.stagePosY + self.tileHeight) % (self.tileHeight * len(self.tiles))) // self.tileHeight, #x2
-    #             self.stagePosX + self.tileWidth) % (self.tileWidth * len(self.tiles[0]))) // self.tileWidth] #y2
-    def _quad(self, x, y):
-        tiles = _tiles(x,y)
-    def _id(self, x, y):
-        return 'images/' + str(self.tileWidth) + '-' + str(self.tileHeight)
-    def _cache(self, x, y):
-        tileX, tileY = _tile(self, x, y)
-        if not self.cache:
-            self.cache[tileX][tileY] = _load()
-    def offset(self, x, y):
-        return (0 - self.stagePosX % self.tileWidth,
-                0 - self.stagePosY % self.tileHeight)
-    def show(self, img, x, y):
-        screen.blit(img,(x,y))
-    # https://github.com/StevePaget/Pygame_Functions/blob/master/pygame_functions.py
+                self._draw_box(screen, x_offset, y_offset, x, y)
