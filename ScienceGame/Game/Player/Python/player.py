fbbd69414c618c838a529c802cc1d9327408d665
Soooham <75951802+Chogum-exe@users.noreply.github.com>
2021-06-21 14:44:46 -0700

    reeeee
    Graphics no work :(

--- ScienceGame/Game/Player/Python/player.py
+++ ScienceGame/Game/Player/Python/player.py
@@ -5,8 +5,9 @@ import os
 class Player:
     def __init__(self, x, y, pygame):
         pygame.init()
-        sheet = SpriteLoader(os.path.join(".","Game","Player","Frames","PlayerFrames.png"), (144, 288))
-        self.sprites = sheet.images_at([(0, 0, 1728, 1728), (0, 1728, 1728, 3456)])
+        self.size = 144
+        sheet = SpriteLoader(os.path.join(".","Game","Player","Frames","PlayerFrames.png"), (self.size*2, self.size))
+        self.sprites = sheet.images_at([(0, 0, 1728, 1728), (0, 1728, 1728, 3456)], -1)
         self.x = x
         self.y = y
         self.pygame = pygame
@@ -18,7 +19,8 @@ class Player:
         self.y += y_dir
 
     def render(self, screen):
-        screen.blit(self.sprites[0], (self.x, self.y))
+        frame = self.pygame.Rect(self.x, self.y, self.size, self.size)
+        screen.blit(self.sprites[0], (self.x, self.y), frame)
     
     def run(self, screen):
         self.move()
